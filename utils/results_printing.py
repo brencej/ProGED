cd8bf5a2e06834656012425d1fee986b619a5d3b
brencej <jure.brence@gmail.com>
2021-10-27 11:13:09 +0200

    count successes

---
+++ utils/results_printing.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 18 14:32:17 2021
+
+@author: jureb
+"""
+
+import numpy as np
+import pandas as pd
+import sys
+import os
+import pickle
+import glob
+
+import ProGED as pg
+
+if __name__ == "__main__":
+    files = glob.glob("*fit.models")
+    N = int(sys.argv[1])
+    
+    outfile = sys.argv[2]
+    successes = 0
+    
+    for file in files:
+        print("---" + file)
+        with open(file, "rb") as f:
+            models = pickle.load(f)
+            
+        best_models = models.retrieve_best_models(N)
+        if len(best_models) > 0:
+            if best_models[0].get_error() < 1e-9:
+                successes += 1
+            
+        with open(outfile, "a") as f:
+            f.write("\n---" + file + ", unique models: " + str(len(models))+"\n")
+            f.write(str(best_models))
+        
+    with open(outfile, "a") as f:
+        f.write("-------- successes: " + str(successes) + " ----------------")
+    print("Successes: ", successes)
+            
+        
+    
