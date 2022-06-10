5fa883d4fb6efeffa9dbc179f81c14b229087f86
brencej <jure.brence@gmail.com>
2022-06-10 10:06:59 +0200

    numeric differentiation experiments

--- ProGED/examples/DS2022/numdiff_proged.py
+++ ProGED/examples/DS2022/numdiff_proged.py
@@ -31,24 +31,29 @@ if __name__ == "__main__":
                         "p_vars": [1/3, 1/3, 1/3], 
                         "functions": [], "p_F": []}
     grammar = pg.grammar_from_template("polynomial", grammar_settings)
+
+    
+    optimizer_settings = {"lower_upper_bounds": (-30,30),
+                            "atol": 0.001, "tol": 0.001}
+    estimation_settings = {"target_variable_index": 0,
+                            "verbosity": 1,
+                            "optimizer_settings": optimizer_settings,
+                            "max_constants": 3}
+
     """ ITERATE THROUGH THE THREE DIMENSIONS """
     t1 = time.time()
     for i in range(3):
         """ SETUP THE MODELS """
-        #models = pg.generate.generate_models(grammar, symbols, strategy_settings={"N":1})
-        models = pg.ModelBox()
-        models.add_model(optimal_model[i], symbols)
+        models = pg.generate.generate_models(grammar, symbols, strategy_settings={"N":500})
+        #models = pg.ModelBox()
+        #models.add_model(optimal_model[i], symbols)
 
         """ FIT PARAMETERS """
-        optimizer_settings = {"lower_upper_bounds": (-30,30),
-                              "atol": 0.001, "tol": 0.001}
-        estimation_settings = {"target_variable_index": 0,
-                                "verbosity": 0,
-                                "optimizer_settings": optimizer_settings}
-
         dat = np.hstack((dX[i].reshape((-1,1)), X))
         models_fit = pg.fit_models(models, dat, task_type="algebraic", 
                                     estimation_settings=estimation_settings)
+        
+        models_fit.dump("numdiff_lorenz_eq" + str(i) + "_models_fit.pg")
         print(models_fit)
     t2 = time.time()
     print("Time: ", t2-t1)
