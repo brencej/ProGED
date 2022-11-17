9a410d688255da338018845e6b9bfb74b5b6a0ab
bogec <bostogec@gmail.com>
2022-11-17 11:38:02 +0100

    persistent homology metric for parameter estimation implemented

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
                         "scipy", 
                         "sympy", 
                         "nltk",
+                        # "scikit-tda",
                         # "scikit-learn",
                         # "hyperopt",
                         # "pytest",
